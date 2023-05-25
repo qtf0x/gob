7239bdd23173ae42e1e6ebc5728d11423e6672e3
Daniel Nau <dnau@mines.edu>
2023-05-25 11:48:22 -0600

    Added zone type to ribbon

--- dialog_box_example.scm
+++ dialog_box_example.scm
@@ -13,6 +13,88 @@
 (make-new-rpvar 'gob/init_poro_input 0 'real)
 (make-new-rpvar 'gob/resist_scalar_input 0 'real)
 
+(make-new-rpvar 'gob/startup_center_radio_button #t 'boolean)
+(make-new-rpvar 'gob/startup_headgate_radio_button #f 'boolean)
+(make-new-rpvar 'gob/startup_tailgate_radio_button #f 'boolean)
+(make-new-rpvar 'gob/gob_center_radio_button #f 'boolean)
+(make-new-rpvar 'gob/mid_headgate_radio_button #f 'boolean)
+(make-new-rpvar 'gob/mid_tailgate_radio_button #f 'boolean)
+(make-new-rpvar 'gob/working_center_radio_button #f 'boolean)
+(make-new-rpvar 'gob/working_headgate_radio_button #f 'boolean)
+(make-new-rpvar 'gob/working_tailgate_radio_button #f 'boolean)
+
+; Zone Selection Definition
+(define zone-selection-box
+	; Let Statement, Local Varaible Declarations
+	(let ((dialog-box #f)
+		(table)
+			(gob/zone_button_box)
+			(gob/startup_center_radio_button)	
+			(gob/startup_headgate_radio_button)
+			(gob/startup_tailgate_radio_button)	
+			(gob/gob_center_radio_button)	
+			(gob/mid_headgate_radio_button)	
+			(gob/mid_tailgate_radio_button)	
+			(gob/working_center_radio_button)	
+			(gob/working_headgate_radio_button)	
+			(gob/working_tailgate_radio_button)		
+	)
+
+		; update-cb - invoked when the dialog box is opened
+		(define (update-cb . args)
+			(cx-set-toggle-button gob/startup_center_radio_button (rpgetvar 'gob/startup_center_radio_button))
+			(cx-set-toggle-button gob/startup_headgate_radio_button (rpgetvar 'gob/startup_headgate_radio_button))
+			(cx-set-toggle-button gob/startup_tailgate_radio_button (rpgetvar 'gob/startup_tailgate_radio_button))
+			(cx-set-toggle-button gob/gob_center_radio_button (rpgetvar 'gob/gob_center_radio_button))
+			(cx-set-toggle-button gob/mid_headgate_radio_button (rpgetvar 'gob/mid_headgate_radio_button))
+			(cx-set-toggle-button gob/mid_tailgate_radio_button (rpgetvar 'gob/mid_tailgate_radio_button))
+			(cx-set-toggle-button gob/working_center_radio_button (rpgetvar 'gob/working_center_radio_button))
+			(cx-set-toggle-button gob/working_headgate_radio_button (rpgetvar 'gob/working_headgate_radio_button))
+			(cx-set-toggle-button gob/working_tailgate_radio_button (rpgetvar 'gob/working_tailgate_radio_button))
+		)
+
+		; apply-cb - invoked when you click "OK"
+		(define (apply-cb . args)
+			(rpsetvar 'gob/startup_center_radio_button (cx-show-toggle-button gob/startup_center_radio_button))
+			(rpsetvar 'gob/startup_headgate_radio_button (cx-show-toggle-button gob/startup_headgate_radio_button))
+			(rpsetvar 'gob/startup_tailgate_radio_button (cx-show-toggle-button gob/startup_tailgate_radio_button))
+			(rpsetvar 'gob/gob_center_radio_button (cx-show-toggle-button gob/gob_center_radio_button))
+			(rpsetvar 'gob/mid_headgate_radio_button (cx-show-toggle-button gob/mid_headgate_radio_button))
+			(rpsetvar 'gob/mid_tailgate_radio_button  (cx-show-toggle-button gob/mid_tailgate_radio_button ))
+			(rpsetvar 'gob/working_center_radio_button (cx-show-toggle-button gob/working_center_radio_button))
+			(rpsetvar 'gob/working_headgate_radio_button (cx-show-toggle-button gob/working_headgate_radio_button))
+			(rpsetvar 'gob/working_tailgate_radio_button (cx-show-toggle-button gob/working_tailgate_radio_button))
+		
+			(%run-udf-apply 1)
+		)
+
+		(lambda args
+			(if (not dialog-box)
+				(let ()
+				
+					(set! dialog-box (cx-create-panel "Zone Selection" apply-cb update-cb))
+					(set! table (cx-create-table dialog-box ""))
+
+					(set! gob/zone_button_box (cx-create-button-box table "Zone Type" 'radio-mode #t 'col 0))
+
+					(set! gob/startup_center_radio_button (cx-create-toggle-button gob/zone_button_box "Startup Center"))
+					(set! gob/startup_headgate_radio_button (cx-create-toggle-button gob/zone_button_box "Startup Headgate"))
+					(set! gob/startup_tailgate_radio_button (cx-create-toggle-button gob/zone_button_box "Startup Tailgate"))
+					(set! gob/gob_center_radio_button (cx-create-toggle-button gob/zone_button_box "Gob Center"))
+					(set! gob/mid_headgate_radio_button (cx-create-toggle-button gob/zone_button_box "Mid-Panel Headgate"))
+					(set! gob/mid_tailgate_radio_button (cx-create-toggle-button gob/zone_button_box "Mid-Panel Tailgate"))
+					(set! gob/working_center_radio_button (cx-create-toggle-button gob/zone_button_box "Working Center"))
+					(set! gob/working_headgate_radio_button (cx-create-toggle-button gob/zone_button_box "Working Headgate"))
+					(set! gob/working_tailgate_radio_button (cx-create-toggle-button gob/zone_button_box "Working Tailgate"))
+
+				) ;End Of Let Statement
+			) ;End Of If Statement
+			;Call To Open Dialog Box
+			(cx-show-panel dialog-box)
+		) ;End Of Args Function
+	) ;End Of Let Statement
+) ; End of Zone Selection Definition
+
 ;Dialog Box Definition
 (define gui-dialog-box
 	;Let Statement, Local Variable Declarations
@@ -67,7 +149,7 @@
 			(if (not dialog-box)
 				(let ()
 				
-					(set! dialog-box (cx-create-panel "Mine Gob" apply-cb update-cb))
+					(set! dialog-box (cx-create-panel "Mine Model and Parameters" apply-cb update-cb))
 					(set! table (cx-create-table dialog-box ""))
 
 					(set! gob/mine_button_box (cx-create-button-box table "Mine Model" 'radio-mode #t 'col 0))
@@ -96,4 +178,5 @@
 (cx-add-menu "Model Mine Gob" #f)
 (cx-add-hitem "Model Mine Gob" "Permeability and Porosity" #f #f #t #f)
 ;Menu Item Added To Above Created "New Menu->New Submenu" Submenu In Fluent
-(cx-add-item "Permeability and Porosity" "Permeability and Porosity" #\U #f #t gui-dialog-box)
+(cx-add-item "Permeability and Porosity" "Mine Model and Parameters" #\U #f #t gui-dialog-box)
+(cx-add-item "Permeability and Porosity" "Zone Selection" #\U #f #t zone-selection-box)
