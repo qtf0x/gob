b879155e81bcc8176f569f1537a0e20e13fc261f
qtf0x <vmarias@mines.edu>
2023-06-08 15:20:40 -0600

    rewrote VSI functions as function-like macros, removed thread dimensions functions that were causing concurrency issues

--- include/utils.h
+++ include/utils.h
@@ -43,44 +43,4 @@ double clamp_positive(const double num);
  */
 double clamp(const double num, const double min, const double max);
 
-/**
- * @brief Calculate the dimensions of the thread with the given ID.
- * 
- * @param [in] t_id Fluent identifier of the thread to be queried
- * @param [out] width total width of the thread (x dimension)
- * @param [out] length total length of the thread (y dimension)
- * @param [out] min_x_out minimum x value of any cell in the thread
- * @param [out] max_x_out maximum x value of any cell in the thread
- * @param [out] min_y_out minimum y value of any cell in the thread
- * @param [out] max_y_out maximum y value of any cell in the thread
- */
-void get_thread_dimensions(const int thread_id, real *const width, real *const length);
-
-/**
- * @brief Locates the x and y offsets (to the working face) for a sub-critical 
- * Trona mine.
- * 
- * @param [out] x_offset delta to the FLAC3D origin (x dimension)
- * @param [out] y_offset delta to the FLAC3D origin (y dimension)
- */
-void get_offsets_sub_critical(real *const x_offset, real *const y_offset);
-
-/**
- * @brief Locates the x and y offsets (to the working face) for a 
- * super-ciritical coal mine.
- * 
- * @param [out] x_offset delta to the FLAC3D origin (x dimension)
- * @param [out] y_offset delta to the FLAC3D origin (y dimension)
- */
-void get_offsets_super_critical(real *const x_offset, real *const y_offset);
-
-/**
- * @brief Locates the x and y offsets (to the working face) for any single-part 
- * mesh (not partitioned into threads).
- * 
- * @param x_offset 
- * @param y_offset 
- */
-void get_offsets_single_part(real *const x_offset, real *const y_offset);
-
 #endif // GOB_UTILS_H
--- src/udf_main.c
+++ src/udf_main.c
@@ -3,7 +3,11 @@
 #include "udf.h" // Fluent macros
 
 #include "udf_vsi.h"
-
+#include "udf_adjust.h"
+#include "udf_explosive_mix.h"
+#include "udf_inertia.h"
+#include "udf_permeability.h"
+#include "udf_porosity.h"
 #include "utils.h"
 
 #define domain_ID 2 // using primary phase domain
@@ -16,14 +20,9 @@ DEFINE_EXECUTE_FROM_GUI(udf_main, longwallgobs, mode)
 	const bool SINGLE_PART_MESH = RP_Get_Integer("longwallgobs/single_part_mesh_id") >= 0;
 
 	// get FLAC3D offsets
-	real panel_x_offset, panel_y_offset;
+	// TODO: 28 new RP variables yay!
 
-	if (SINGLE_PART_MESH)
-		get_offsets_single_part(&panel_x_offset, &panel_y_offset);
-	else if (RP_Get_Boolean("longwallgobs/mine_T_radio_button"))
-		get_offsets_sub_critical(&panel_x_offset, &panel_y_offset);
-	else
-		get_offsets_super_critical(&panel_x_offset, &panel_y_offset);
+	printf("panel_x_offset: %f\npanel_y_offset: %f\n", panel_x_offset, panel_y_offset);
 
 	// calculate vsi
 	if (RP_Get_Boolean("longwallgobs/mine_C_radio_button"))
