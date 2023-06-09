edacc099864a082908bf392e1b4c42101ef9c525
qtf0x <vmarias@mines.edu>
2023-06-09 12:25:28 -0600

    getting x and y offsets works!

--- src/udf_main.c
+++ src/udf_main.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <math.h>
 
 #include "udf.h" // Fluent macros
 
@@ -20,15 +21,32 @@ DEFINE_EXECUTE_FROM_GUI(udf_main, longwallgobs, mode)
 	const bool SINGLE_PART_MESH = RP_Get_Integer("longwallgobs/single_part_mesh_id") >= 0;
 
 	// get FLAC3D offsets
-	// TODO: 28 new RP variables yay!
+	real panel_x_offset = 0;
+	real panel_y_offset = 0;
+
+	if (SINGLE_PART_MESH) {
+		panel_x_offset = (RP_Get_Real("longwallgobs/single_part_mesh_max_x") -
+				  RP_Get_Real("longwallgobs/single_part_mesh_min_x")) /
+				 2;
+		panel_y_offset = RP_Get_Real("longwallgobs/single_part_mesh_max_y");
+	} else if (RP_Variable_Exists_P("longwallgobs/mine_T_radio_button") &&
+		   RP_Get_Boolean("longwallgobs/mine_T_radio_button")) {
+		panel_x_offset = RP_Get_Real("longwallgobs/working_face_corner_max_x");
+		panel_y_offset = RP_Get_Real("longwallgobs/working_face_corner_max_y");
+	} else {
+		panel_x_offset = (RP_Get_Real("longwallgobs/working_face_center_max_x") -
+				  RP_Get_Real("longwallgobs/working_face_center_min_x")) /
+				 2;
+		panel_y_offset = RP_Get_Real("longwallgobs/working_face_center_max_y");
+	}
 
 	printf("panel_x_offset: %f\npanel_y_offset: %f\n", panel_x_offset, panel_y_offset);
 
 	// calculate vsi
-	if (RP_Get_Boolean("longwallgobs/mine_C_radio_button"))
-		vsi_mine_C_stepped(SINGLE_PART_MESH, panel_x_offset, panel_y_offset);
-	else if (RP_Get_Boolean("longwallgobs/mine_E_radio_button"))
-		vsi_mine_E_stepped(SINGLE_PART_MESH, panel_x_offset, panel_y_offset);
-	else if (RP_Get_Boolean("longwallgobs/mine_T_radio_button"))
-		vsi_trona_stepped(SINGLE_PART_MESH, panel_x_offset, panel_y_offset);
+	// if (RP_Get_Boolean("longwallgobs/mine_C_radio_button"))
+	// 	vsi_mine_C_stepped(SINGLE_PART_MESH, panel_x_offset, panel_y_offset);
+	// else if (RP_Get_Boolean("longwallgobs/mine_E_radio_button"))
+	// 	vsi_mine_E_stepped(SINGLE_PART_MESH, panel_x_offset, panel_y_offset);
+	// else if (RP_Get_Boolean("longwallgobs/mine_T_radio_button"))
+	// 	vsi_trona_stepped(SINGLE_PART_MESH, panel_x_offset, panel_y_offset);
 }
